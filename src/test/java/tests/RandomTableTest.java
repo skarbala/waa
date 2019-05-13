efdc8087f4316ded3b9f62e2f4a31fb35a170ef8
martin.skarbala <skarbala.martin@gmail.com>
2019-05-13 18:33:55 +0200

    pridany test na overenie ci meno je vyplnene

---
+++ src/test/java/tests/RandomTableTest.java
@@ -0,0 +1,62 @@
+package tests;
+
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class RandomTableTest {
+  WebDriver driver;
+
+  @Before
+  public void setUp() {
+    System.setProperty("webdriver.chrome.driver", "chromedriver74.exe");
+    //0.spustit prehliadac
+    driver = new ChromeDriver();
+    //1.otvorit stranku
+    driver.get("http://localhost:8888/tabulka.php");
+  }
+
+  @Test
+  public void itShouldPrintLastRow() {
+    //2.najdem a vypisem posledny riadok
+    System.out.println(driver.findElement(By.xpath("//table/tbody/tr[last()]")).getText());
+
+    //3.najdem a vypisem meno z predposledneho riadku
+    System.out.println(driver.findElement(By.xpath("//table/tbody/tr[last()-1]/td[2]")).getText());
+
+  }
+
+  @Test
+  public void itShouldContainDataForEachRow() {
+    List<WebElement> tableRows = driver.findElements(By.cssSelector("table tbody tr"));
+    for (WebElement tableRow : tableRows) {
+      Assert.assertFalse(tableRow.getText().isEmpty());
+    }
+  }
+
+  @Test
+  public void itShouldContainNameForEachRow() {
+    List<WebElement> tableRows = driver.findElements(By.cssSelector("table tbody tr"));
+    for (WebElement tableRow : tableRows) {
+      tableRow.findElement(By.cssSelector("td:nth-child(2)"));
+      WebElement rowName = tableRow.findElement(By.xpath("./td[2]"));
+      Assert.assertFalse(rowName.getText().isEmpty());
+
+    }
+  }
+
+    @After
+    public void tearDown(){
+      //zatvorit prehliadac
+      driver.close();
+      //ukoncit session
+      driver.quit();
+    }
+}

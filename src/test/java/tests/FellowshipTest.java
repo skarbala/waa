5572e79f8e761ae7b64f492693c6eca465c47fe7
martin.skarbala <skarbala.martin@gmail.com>
2019-05-13 19:49:10 +0200

    pridany test pre overenie mien jednotlivych fellows z ringu

---
+++ src/test/java/tests/FellowshipTest.java
@@ -0,0 +1,54 @@
+package tests;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FellowshipTest {
+  WebDriver driver;
+
+  @Before
+  public void setUp() {
+    System.setProperty("webdriver.chrome.driver", "chromedriver74.exe");
+    //0.spustit prehliadac
+    driver = new ChromeDriver();
+    //1.otvorit stranku
+    driver.get("http://localhost:8888/fellowship.php");
+  }
+
+  @Test
+  public void itShouldContainNameForEachFellow() {
+    List<WebElement> fellowElements = driver.findElements(By.cssSelector("ul.list-of-fellows li"));
+
+    for (WebElement fellowElement : fellowElements) {
+      Assert.assertFalse(fellowElement.findElement(By.cssSelector("h1")).getText().isEmpty());
+    }
+  }
+
+  @Test
+  public void itShouldContainSpecifiedFellows() {
+    //najdem si zoznam elementov (kachliciek)
+    List<WebElement> fellowElements = driver.findElements(By.cssSelector("ul.list-of-fellows li"));
+    //predpripravim si zoznam stringov do ktoreho si ulozim jednotlive mena
+    List<String> fellowNames = new ArrayList<String>();
+
+    //prejdem zoznam elementov, kachliciek
+    for (WebElement fellowElement : fellowElements) {
+      System.out.println(fellowElement.findElement(By.cssSelector("h1")).getText());
+      //v ramci kazdej kachlicky si najdem meno a to ulozim do zoznamu mien
+      fellowNames.add(fellowElement.findElement(By.cssSelector("h1")).getText());
+    }
+    System.out.println(fellowNames);
+    //over ze list obsahuje Gandalf-a, a Aragorn-a
+    Assert.assertTrue(fellowNames.contains("Gandalf"));
+    Assert.assertTrue(fellowNames.contains("Aragorn"));
+    Assert.assertTrue(fellowNames.contains("Frodo"));
+  }
+}

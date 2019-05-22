a272c96cb29980d57920a8b889067c4032663224
martin.skarbala <skarbala.martin@gmail.com>
2019-05-22 18:49:21 +0200

    refaktorizacia, vynatie vytvorenia GosslingatorPage do casti before

--- src/test/java/tests/GosslingatorTest.java
+++ src/test/java/tests/GosslingatorTest.java
@@ -9,24 +9,21 @@ import base.TestBase;
 import pages.GosslingatorPage;
 
 public class GosslingatorTest extends TestBase {
-
+  private GosslingatorPage gossPage;
   private String actualNumberOfRyans; //private = viditelna len pre triedu
 
   @Before
   public void openPage() {
     //1.otvorit stranku
     driver.get(BASE_URL + "/gosslingator.php");
+    gossPage = new GosslingatorPage(driver);
   }
 
   @Test
   public void itShouldAddOneRyan() {
-    GosslingatorPage gossPage = new GosslingatorPage(driver);
-    //2.kliknut na tlacidlo pridat
     gossPage.addRyan();
-    //3.overit pocitanie ryanov
+
     Assert.assertEquals("1", gossPage.getRyanCounterNumber());
-    // vypisem si do console aktualny pocet z pocitadla ryanov
-    Assert.assertEquals("ryan", gossPage.getCounterDescription());
   }
 
   @Test
